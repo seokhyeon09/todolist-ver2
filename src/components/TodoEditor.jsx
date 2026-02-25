5a8c60c9aafc01b22e8a48a7769ef9ed642f5ee1
seokhyeon09 <ring4392@gmail.com>
2026-02-25 10:34:48 +0900

    update

--- src/components/TodoEditor.jsx
+++ src/components/TodoEditor.jsx
@@ -6,16 +6,16 @@ const TodoEditor = ({ onCreate }) => {
   const onChangeContent = (e) => {
     setContent(e.target.value)
   }
-  const onSubmit=()=>{
-    if(content==''){
-      inputRef.current.fcous()
+  const onSubmit = () => {
+    if (content == '') {
+      inputRef.current.focus()
       return
     }
     onCreate(content)
     setContent("")
   }
-  const onKeyDown =(e)=>{
-    if(e.keyCode===13){
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
       onSubmit()
     }
   }
